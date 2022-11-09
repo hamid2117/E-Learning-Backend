056f62b35cc908af59e6a6e6f0028838c32fce62
hamid2117 <saim008865@gmail.com>
2022-11-09 15:32:07 +0500

    done setting index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-// const app = require('./app')
 import app from './app.js'
 
 // const port = 5000
 const port = process.env.PORT || 5000
+
 app.listen(port, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} : http://localhost:${port}`
